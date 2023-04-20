487807e14384e5c71a039758aba68e239937cbb4
Sotatek-HaiNguyen4 <hai.nguyen4@sotatek.com>
2023-04-20 15:16:18 +0700

    Fix router page index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import usePushNotifications from './usePushNotifications';
 import './App.css';
 import AddToHomeScreen from '@ideasio/add-to-homescreen-react';
+import Notify from './Notify';
 
 function App() {
   const {
@@ -46,58 +48,66 @@ function App() {
 
   }, [pushNotificationSupported, isConsentGranted, userSubscription, pushServerSubscriptionId])
 
+  // check router
+  const pathname = window.location.pathname
 
   return (
-    <div className='App'>
-      <AddToHomeScreen skipFirstVisit={false} displayPace={0} mustShowCustomPrompt={true} />
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Loading loading={loading} />
+    <div>
+      {
+        pathname === '/notify' ? <Notify /> : <div className='App'>
+          <AddToHomeScreen skipFirstVisit={false} displayPace={0} mustShowCustomPrompt={true} />
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <Loading loading={loading} />
 
-        <p>Push notification are {!pushNotificationSupported && 'NOT'} supported by your device.</p>
+            <p>Push notification are {!pushNotificationSupported && 'NOT'} supported by your device.</p>
 
-        <p>
-          User consent to recevie push notificaitons is <strong>{userConsent}</strong>.
-        </p>
+            <p>
+              User consent to recevie push notificaitons is <strong>{userConsent}</strong>.
+            </p>
 
-        <Error error={error} />
+            <Error error={error} />
 
-        {/* <button
+            {/* <button
           disabled={!pushNotificationSupported || isConsentGranted}
           onClick={onClickAskUserPermission}>
           {isConsentGranted ? 'Consent granted' : ' Ask user permission'}
         </button> */}
-        <span>{isConsentGranted ? 'Consent granted' : ' Ask user permission'}</span>
+            <span>{isConsentGranted ? 'Consent granted' : ' Ask user permission'}</span>
 
-        {/* <button
+            {/* <button
           disabled={!pushNotificationSupported || !isConsentGranted || userSubscription}
           onClick={onClickSusbribeToPushNotification}>
           {userSubscription ? 'Push subscription created' : 'Create Notification subscription'}
         </button> */}
-        <span>{userSubscription ? 'Push subscription created' : 'Create Notification subscription'}</span>
+            <span>{userSubscription ? 'Push subscription created' : 'Create Notification subscription'}</span>
 
 
-        {/* <button
+            {/* <button
           disabled={!userSubscription || pushServerSubscriptionId}
           onClick={onClickSendSubscriptionToPushServer}>
           {pushServerSubscriptionId
             ? 'Subscrption sent to the server'
             : 'Send subscription to push server'}
         </button> */}
-        <span>
-          {pushServerSubscriptionId
-            ? 'Subscrption sent to the server'
-            : 'Send subscription to push server'}
-        </span>
+            <span>
+              {pushServerSubscriptionId
+                ? 'Subscrption sent to the server'
+                : 'Send subscription to push server'}
+            </span>
 
-        {pushServerSubscriptionId && (
-          <div>
-            <p>The server accepted the push subscrption!</p>
-            <button onClick={onClickSendNotification}>Send a notification</button>
-          </div>
-        )}
-      </header>
+            {pushServerSubscriptionId && (
+              <div>
+                <p>The server accepted the push subscrption!</p>
+                <button onClick={onClickSendNotification}>Send a notification</button>
+              </div>
+            )}
+          </header>
+        </div>
+      }
+
     </div>
+
   );
 }
 
